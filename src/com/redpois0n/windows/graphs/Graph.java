6f2849efce37dadf13740e8601e8fe245212bbeb
redpois0n <red@cmail.nu>
2013-10-14 18:40:15 +0200

    First rect

--- src/com/redpois0n/windows/graphs/Graph.java
+++ src/com/redpois0n/windows/graphs/Graph.java
@@ -1,8 +1,22 @@
 package com.redpois0n.windows.graphs;
 
-import java.awt.Component;
+import java.awt.Color;
+import java.awt.Graphics;
 
-public class Graph extends Component {
+import javax.swing.JComponent;
+
+@SuppressWarnings("serial")
+public class Graph extends JComponent {
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		//draw first background rectangle
+		g.setColor(Color.gray);
+		g.drawRect(0, 0, 100, 200);
+		
+		
+		g.dispose();
+	}
 
 
 }

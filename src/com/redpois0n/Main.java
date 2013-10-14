7d4b2891dfe157161aa8145c58de321dff842080
redpois0n <red@cmail.nu>
2013-10-14 18:44:16 +0200

    static final colors, inner border

--- src/com/redpois0n/Main.java
+++ src/com/redpois0n/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-		frame.setSize(450, 300);
+		frame.setSize(750, 450);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
--- src/com/redpois0n/windows/graphs/Graph.java
+++ src/com/redpois0n/windows/graphs/Graph.java
@@ -8,11 +8,18 @@ import javax.swing.JComponent;
 @SuppressWarnings("serial")
 public class Graph extends JComponent {
 	
+	public static final Color BORDER_COLOR = Color.gray;
+	public static final Color INNER_COLOR = Color.black;
+	
 	@Override
 	public void paintComponent(Graphics g) {
 		//draw first background rectangle
-		g.setColor(Color.gray);
-		g.drawRect(0, 0, 100, 200);
+		g.setColor(BORDER_COLOR);
+		g.drawRect(0, 0, 100, 250);
+		g.drawRect(105, 0, 550, 250);
+		
+		//draw inner color
+		g.setColor(INNER_COLOR);
 		
 		
 		g.dispose();

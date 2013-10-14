1db93448c17e8184c5f10f61c711f6a7129ca176
redpois0n <red@cmail.nu>
2013-10-14 18:33:09 +0200

    Debug start

--- src/com/redpois0n/Main.java
+++ src/com/redpois0n/Main.java
@@ -1,10 +1,15 @@
 package com.redpois0n;
 
+import javax.swing.JFrame;
+
 public class Main {
 
-
 	public static void main(String[] args) {
-		
+		JFrame frame = new JFrame();
+		frame.setSize(450, 300);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 
 }

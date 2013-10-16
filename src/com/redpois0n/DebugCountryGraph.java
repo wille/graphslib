93573b77a8c24918575f1a99d491c08b76ae75c9
redpois0n <red@cmail.nu>
2013-10-16 20:43:43 +0200

    CountryGraph begin

---
+++ src/com/redpois0n/DebugCountryGraph.java
@@ -0,0 +1,13 @@
+package com.redpois0n;
+
+public class DebugCountryGraph {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/redpois0n/graphs/CountryGraph.java
@@ -0,0 +1,14 @@
+package com.redpois0n.graphs;
+
+import java.awt.Graphics;
+
+import javax.swing.JComponent;
+
+public class CountryGraph extends JComponent {
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		
+	}
+
+}

c88c432e0026133f1c0f0be8c688c6c1e27c0e46
redpois0n <red@cmail.nu>
2015-03-28 10:12:39 +0100

    Remove 404 icon

--- src/com/redpois0n/graphs/utils/IconUtils.java
+++ src/com/redpois0n/graphs/utils/IconUtils.java
@@ -9,12 +9,6 @@ public class IconUtils {
 
 	private static final Map<String, ImageIcon> CACHE = new HashMap<String, ImageIcon>();
 
-	public static final ImageIcon NO_ICON;
-
-	static {
-		NO_ICON = getIcon("404");
-	}
-
 	public static ImageIcon getIcon(String path) {
 		ImageIcon icon = null;
 
@@ -25,7 +19,6 @@ public class IconUtils {
 				icon = new ImageIcon(IconUtils.class.getResource("/icons/" + path + ".png"));
 			} catch (Exception e) {
 				e.printStackTrace();
-				icon = NO_ICON;
 			}
 
 			CACHE.put(path, icon);

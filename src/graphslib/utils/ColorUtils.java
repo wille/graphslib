b23dc9511cacef1ab3be5bdc6e93f92258aae0b7
redpois0n <red@cmail.nu>
2015-11-02 19:43:07 +0100

    get common color from image

--- src/graphslib/utils/ColorUtils.java
+++ src/graphslib/utils/ColorUtils.java
@@ -1,20 +1,37 @@
 package graphslib.utils;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 
 public class ColorUtils {
 
+	public static Color getCommonColor(Image aimage) {
+		if (aimage instanceof BufferedImage) {
+			return getCommonColor((BufferedImage) aimage);
+		}
+		
+		BufferedImage image = new BufferedImage(aimage.getWidth(null), aimage.getHeight(null), BufferedImage.TYPE_INT_RGB);
+
+		Graphics g = image.createGraphics();
+		g.drawImage(aimage, 0, 0, null);
+		g.dispose();
+		
+		return getCommonColor(image);
+	}
+
 	/**
 	 * Returns the common color for the image
+	 * 
 	 * @param image
 	 * @return
 	 */
-	public static Color getCommonColor(BufferedImage image) {		
+	public static Color getCommonColor(BufferedImage image) {
 		int r = 0;
 		int g = 0;
 		int b = 0;
-		
+
 		for (int x = 0; x < image.getWidth(); x++) {
 			for (int y = 0; y < image.getHeight(); y++) {
 				int pixel = image.getRGB(x, y);
@@ -24,12 +41,12 @@ public class ColorUtils {
 				b += c.getBlue();
 			}
 		}
-		
+
 		int total = image.getWidth() * image.getHeight();
 		r /= total;
 		g /= total;
 		b /= total;
-		
+
 		return new Color(r, g, b);
 	}
 }
